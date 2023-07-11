9053f4dfdaae2c1c6d75748400237e8d25faf9c3
Felipe Borges <felipe.souza1@universo.univates.br>
2023-07-11 10:14:35 -0300

    att

---
+++ build/precache-manifest.7338db968dec1dc2182eaeca42b6c8a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828636f17cb35ba7c845c9c6a76899c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401e18e71c80b5a27009",
+    "url": "/static/css/main.f3a96aa0.chunk.css"
+  },
+  {
+    "revision": "959d25930b8a40bbc777",
+    "url": "/static/js/2.3a43f369.chunk.js"
+  },
+  {
+    "revision": "fbec3ef3c7cc3ed2e44fea7fcdd95b5e",
+    "url": "/static/js/2.3a43f369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401e18e71c80b5a27009",
+    "url": "/static/js/main.e8cefea7.chunk.js"
+  },
+  {
+    "revision": "642526f45c40b4658273",
+    "url": "/static/js/runtime-main.6359f555.js"
+  },
+  {
+    "revision": "51d79cc0ad995d30f1bca4c10c0676ab",
+    "url": "/static/media/Gotham-Bold.51d79cc0.otf"
+  },
+  {
+    "revision": "aa39a0716156d167d6415f018ce6f2eb",
+    "url": "/static/media/Gotham-Light.aa39a071.otf"
+  }
+]);
