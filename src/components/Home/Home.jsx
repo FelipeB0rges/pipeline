30488043b07c7083ff72ea3fc5ef30d9e558e122
Felipe Borges <felipe.souza1@universo.univates.br>
2023-07-11 08:42:38 -0300

    att

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.scss'
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
+
+
+const Home = () => {
+
+    return (
+        <>
+            <div className="home">
+                <div className="git" onClick={() => {
+                    window.location.href = 'https://github.com/FelipeB0rges'
+                }}>
+                    <GitHubIcon fontSize="40px">
+                    </GitHubIcon>
+                    <div className="nome">
+                        FelipeB0rges
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Home;
